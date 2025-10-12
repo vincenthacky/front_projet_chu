5286339594f300da5ab0e7fc38db8ac3308a79b0
macbook <macbook@MacBook-Pro-ASSO.local>
2025-10-12 00:48:31 +0000

    hebergg

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.elokatecheznous.com/api',
-  storageUrl: 'https://api.elokatecheznous.com/storage'
+  storageUrl: 'https://api.elokatecheznous.com/public/storage'
 };
